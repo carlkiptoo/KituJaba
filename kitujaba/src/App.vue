49ba037f49a2f17efc0f56b0a02da42b558e83ce
carlkiptoo <carlosiamcarl@gmail.com>
2025-06-06 14:55:25 +0300

    Initial file setup

---
+++ kitujaba/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <NavBar />
+<section id='home'><Home /></section>
+<section id='about'><About /></section>
+<section id='product'><Product /></section>
+<section id='contact'><Contact /></section>
+</div>
+
+</template>
+
+<script setup>
+import NavBar from './components/NavBar.vue'
+import Home from './pages/HomePage.vue'
+import About from './pages/AboutUs.vue'
+import Product from './pages/OurProducts.vue'
+import Contact from './pages/ContactUs.vue'
+
+</script>
